1f1e576f065a0cb468a3d78da26d47779185f071
5gizemyuksel <gizemyukselelk@gmail.com>
2024-08-18 10:29:52 +0300

    düzenleme

---
+++ 02 C#  Programlama/18.08.2024/Proje03_Variables/Proje03_Variables/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Proje03_Variables;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //  degiskenAdi=56;
+    //    int degiskenAdi2;
+    //    System.Console.WriteLine(degiskenAdi);
+    // evrensel: Line Comment command+ K C   uncomment Command+ K U 
+
+// tam sayılar);
+sbyte studentpoint=78;//-128 ile 127 arasındadır 1 byte yer kaplar (8 bit)
+System.Console.WriteLine(studentpoint);
+    }
+} 
