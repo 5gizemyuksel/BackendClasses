5a8fe198581277725d25eb03414864c5cad99c7c
5gizemyuksel <gizemyukselelk@gmail.com>
2024-08-24 15:00:53 +0300

    learning methods

---
+++ 02 C#  Programlama/WeeK03/24.08.2024/Project05_VariableSamples/Program.cs
@@ -0,0 +1,104 @@
+﻿using System.Net.Http.Headers;
+
+namespace Project05_VariableSamples;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        #region Yaş Hesaplama
+        // //Kullanıcıdan doğum yılını alıp yaşını hesaplayacak ve bunu ekrana yazdıracak kodu hazırlıyoruz.
+        // Console.Write("Doğum yılınızı giriniz: ");
+        // string input = Console.ReadLine(); // "1975" inputun içinde öyle gözükücek sayısal bir değer olarak gözükmeyecek.
+        // int birthYear = int.Parse(input); // burda sayısal bir değer kazandı dönüştü
+        // int calculatedAge = DateTime.Now.Year - birthYear; // Date.Time tarihi verir Now şu anı salisesine kadar yearda bunun içinden seneyi
+        // string message= "Yaşınız: "+ calculatedAge ;
+        // Console.WriteLine(message);
+
+
+
+
+        #endregion
+        #region Ürün Fiyat Hesaplama
+        //Bir ürünün fiyatı girirldiğinde, onun KDV dahil fiyatını hesaplayıp hem ham hem de KDV dahil fiyatını ekrana yazdıracağız.
+
+        // Console.Write("Ürün fiyatını giriniz: ");
+        // decimal productPrice = decimal.Parse(Console.ReadLine());
+        // double vatRate = 0.18;
+        // double vat = (double)productPrice * vatRate;
+        // decimal totalProductPrice = productPrice + (decimal)vat;
+
+        // string resultMessage = $@"
+        // Ürün Fiyatı    : {productPrice}
+        // KDV            : {vat}
+        // // KDV Dahil Fiyat:{totalProductPrice}";
+        // // Console.WriteLine(resultMessage); // tek bir stringte alt alta sonuç yazdırmak için
+
+        // string resultMessage =$"Ürün Fiyatını giriniz:"{productPrice} //EKSİK KALDI
+        #endregion
+
+        #region Sıcaklık Dönüştürme 
+        // Kullanıcıdan Celsius cinsinden alınan sıcaklık değerini, Fahrenheit cinsine çevirip ekrana her ikisini de alt alta yazdıran kodu hazırlıyoruz. 
+
+        // Console.Write("Celcius Değerini Giriniz: ");
+        // decimal Celsiusdeger= decimal.Parse(Console.ReadLine());
+        // double fahrenheit = ((double)Celsiusdeger*9/5)+32;
+
+
+        // string resultMessage = $@"
+        // Celsius Değeri: {Celsiusdeger}
+        // Fahrenheit Değeri: {fahrenheit}";
+
+        // System.Console.WriteLine(resultMessage); //benim yazdığım
+
+        // System.Console.Write("Celsius sıcaklık değerini giriniz: ");
+        // double celsius= double.Parse(Console.ReadLine());
+
+        // double fahrenheit = (celsius * 1.8) + 32;
+        // string resultMessage = $"Celsius: {Convert.ToInt32(celsius)}\nFahrenheit: {Convert.ToInt32(fahrenheit)}";
+        // Console.Clear();
+        // Console.WriteLine("Sonuç:");
+        // System.Console.WriteLine("----");
+        // System.Console.WriteLine(resultMessage); // $ işaretini düzenli yazmak için kullanıyoruz.
+
+
+
+
+
+
+
+
+        #endregion
+        #region Ağırlık Dönüştürme 
+        // Kullanıcının girdiği kg cinsinden ağırlığı gram'a çevirip sonucu ekrana yazıdıran kodu hazırlayınız.
+        //Not: küsüratlı değerler girilebilri
+        // not: sonuç ekranında hem kg hem gr değerlerini düzgünce göstermeliiniz.
+
+        // System.Console.Write("Kilogram değerini giriniz: ");
+        // double kgDegeri = double.Parse(Console.ReadLine());
+
+        // double grDegeri = kgDegeri * 1000 ;
+
+
+        // string resultMessage= $@"
+        // Sonuç
+        // -----
+        // Kilogram: {kgDegeri} 
+        // Gram: {grDegeri}
+        // ";
+        // System.Console.WriteLine(resultMessage);
+
+        #endregion
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ 02 C#  Programlama/WeeK03/24.08.2024/Project06_StringMethods/Program.cs
@@ -0,0 +1,88 @@
+﻿namespace Project06_StringMethods;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // string fullName = "halil Umut MELER";
+
+        // int len =  fullName.Length;
+        // // System.Console.WriteLine(len);
+        // System.Console.WriteLine(fullName.ToLower());
+        // System.Console.WriteLine(fullName.ToUpper());
+
+        //         string address1= "Yeni, Umut Sitesi E Blok";
+        //         string address2= "No: 12 D: 5";
+        //         string district = "Kdıköy";
+        //         string province= "İstanbul";
+
+        //         string fullAddress1 = address1 + " " + address2 + " " + district + "/ " + province;
+
+        //   System.Console.WriteLine(fullAddress1);
+        //   string fullAddress2 = $"{address1} {address2} {district}/{province}";
+        //   System.Console.WriteLine(fullAddress2);
+
+        //     string text1 ="Nişantaşı";
+        //     string text2 ="Üniversitesi";
+        //     string loc1 = "Bayrampaşa";
+        //     string currentLoc ="Sarıyer";
+        //     string date1= "09.09.2009";
+        //     string date2="05.05.2018";
+        //     //Nişantaşı Üniversitesi 09.09.2009 tARİHİNDE iSTANBUL'UN bAYRAMPAŞA iLÇESİNDE fAAALİYETE bAŞLADI. Ardından 05.05.2018 tarihinde yine İstanbul'un Sarıyer ilçesinde bulunan tek kampüse taşındı. 
+
+        //     string challenge = text1 + " " + text2 +" " + " " + date1 + " " + "tarihinde" + " "+ "İstanbul'un"+ " " + loc1 + " " + "ilçesinde" + " " + "faaliyete başladı." + "Ardından "+date2 + " " + "tarihinde yine İstanbul'un "+ currentLoc + " " + "ilçesinde bulunan tek kampüse taşındı." ;
+        //   System.Console.WriteLine(challenge);
+
+        //   string challenge2 = $"{text1 } {text2} {date1} tarihinde İstanbul'un {loc1} ilçesinde faaliyete başladı. Ardından {date2} tarihinde yine İstanbul'un {currentLoc} ilçesinde bulunan Tech kampüse taşındı." ;
+        //   System.Console.WriteLine(challenge2);
+
+        //   string challenge3 = String.Concat(text1, " ", text2, ", ", date1, " tarihinde İstanbul'un ", loc1, " ilçesinde faaliyete başladı. Ardından ", date2, " tarihinde yine İstanbul'un " , currentLoc, " ilçesinde bulunan Tech kampüse taşındı." );
+        //   System.Console.WriteLine(challenge3);
+
+        // string fullName = "halil Umut MELER";
+        // string findText= "umut";
+        // bool response = fullName.ToLower().Contains(findText.ToLower());
+        // System.Console.WriteLine(response);
+
+        // string firstName = "Begüm";
+        // System.Console.WriteLine(firstName.IndexOf("a"));
+        //  string firstName= "Ayşen Umay";
+        //  System.Console.WriteLine(firstName.StartsWith("A"));
+        // System.Console.WriteLine(firstName.EndsWith("ay"));
+
+        // string newsTitle ="Dolar'ın ateşi çıktı!";
+        // string newsId = "15698U44";
+        // string newsCategory= "Ekonomi";
+        // string newsUrl = newsTitle.ToLower();
+        // newsUrl= newsTitle.Replace(" ","-"); //Dolar'ın-ateşi-çıktı!
+        // newsUrl= newsUrl.Replace("'","");
+        // newsUrl = newsUrl.Replace("!","");
+        // newsUrl = newsUrl.Replace("ı'","i");
+        // newsUrl = newsUrl.Replace("ş", "s");  
+        // newsUrl = newsUrl.Replace("ç", "c");
+        // newsUrl = $
+
+
+
+
+
+        System.Console.WriteLine(newsUrl);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+}
