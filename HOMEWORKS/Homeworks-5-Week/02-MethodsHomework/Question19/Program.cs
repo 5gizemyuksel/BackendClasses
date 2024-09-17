a2b80b166aa767669651fbadbf40f4f1e163aa8d
5gizemyuksel <gizemyukselelk@gmail.com>
2024-09-18 00:16:14 +0300

    Düzenleme

--- HOMEWORKS/Homeworks-5-Week/02-MethodsHomework/Question19/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Question19;
-
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
-    }
-}
--- HOMEWORKS/Homeworks-6-Week/Library Application/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Library_Application;
-
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
-    }
-}
